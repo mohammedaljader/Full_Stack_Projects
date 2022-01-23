9281a7dd9fa9285a1409d43d404dd89308cc4e24
Mohammed Aljader <461166@student.fontys.nl>
2022-01-23 19:59:24 +0100

    backend websocket added

---
+++ WebsocketsAssignment/Backend-websocket/src/main/java/net/javaguides/springboot/websocket/controller/GreetingController.java
@@ -0,0 +1,24 @@
+package net.javaguides.springboot.websocket.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.util.HtmlUtils;
+
+import java.io.*;
+import java.lang.Thread;
+import net.javaguides.springboot.websocket.model.Greeting;
+import net.javaguides.springboot.websocket.model.HelloMessage;
+
+@Controller
+public class GreetingController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+
+}
